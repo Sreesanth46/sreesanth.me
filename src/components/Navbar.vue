29c334fd5b477ff499072d347444c68f363598e2
Sreesanth46 <sreesanth46@gmail.com>
2023-05-22 17:05:21 +0530

    feat: toggle switch, spacing

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,25 +1,30 @@
 <template>
-    <nav class="w-full fixed top-0 bg-white">
+    <nav class="w-full fixed top-0 bg-white z-10">
         <div class="container mx-auto py-5 flex items-center justify-between">
-            <div class="flex items-center gap-2">
+            <div class="hidden sm:flex items-center gap-2">
                 <!-- Logo -->
                 <span class="text-2xl font-medium text-indigo-900"
                     >Sreesanth</span
                 >
             </div>
 
-            <div class="hidden md:flex space-x-8 text-gray-600 font-medium">
+            <div
+                class="hidden md:flex space-x-8 text-gray-600 font-medium items-center justify-center"
+            >
                 <ul v-for="(item, index) in navMenu" :key="index">
                     <li class="hover:text-gray-400">
                         {{ item.name }}
                     </li>
                 </ul>
+                <Theme />
             </div>
         </div>
     </nav>
 </template>
 
 <script setup>
+import Theme from "./Theme.vue";
+
 const navMenu = [
     {
         name: "Home",
@@ -34,7 +39,7 @@ const navMenu = [
     },
 
     {
-        name: "Contact",
+        name: "Contact Me",
     },
 ];
 </script>
