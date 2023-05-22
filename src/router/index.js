9b99ec016741dca2faffd2ba68749311574f5c23
Sreesanth46 <sreesanth46@gmail.com>
2023-05-22 17:45:37 +0530

    feat: home route

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Navbar from '../components/Navbar.vue';
+import Home from '../views/Home.vue';
 
 const routes = [
 	{
 		path: '/',
-		name: 'navbar',
-		component: Navbar,
+		name: 'home',
+		component: Home,
 	}
 ]
 
