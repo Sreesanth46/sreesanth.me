abd67d932d73546c290f8f5e857d5283d8710912
Sreesanth46 <sreesanth46@gmail.com>
2023-05-22 12:25:43 +0530

    fix: container, color - theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,11 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                "weather-primary": "#00668A",
-                "weather-secondary": "#004E71"
+
             }
         },
         fontFamily: {
             Roboto: ["Roboto, sans-serif"]
-        },
-        container: {
-            padding: "2rem",
-            center: true
         },
         screens: {
             sm: "480px",
