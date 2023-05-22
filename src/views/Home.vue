d4c76e5e76726be884402fc8317d9b9152ee8da6
Sreesanth46 <sreesanth46@gmail.com>
2023-05-22 17:45:27 +0530

    feat: home component

---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="relative">
+        <Navbar />
+        <About />
+    </div>
+</template>
+
+<script setup>
+import Navbar from "../components/Navbar.vue";
+import About from "../components/About.vue";
+</script>
