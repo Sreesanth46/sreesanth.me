c531ecd8927c42a909e638c3e91a0839805d10b2
Sreesanth46 <sreesanth46@gmail.com>
2023-05-23 16:27:33 +0530

    feat: about component

---
+++ src/components/About.vue
@@ -0,0 +1,17 @@
+<template>
+    <section class="container">
+        <div class="grid mx-auto">
+            <div class="p-5 space-y-6">
+                <GithubIconVue />
+                <InstagramIconVue />
+                <LinkedinIconVue />
+            </div>
+        </div>
+    </section>
+</template>
+
+<script setup>
+import GithubIconVue from "../icons/GithubIcon.vue";
+import InstagramIconVue from "../icons/InstagramIcon.vue";
+import LinkedinIconVue from "../icons/LinkedinIcon.vue";
+</script>
