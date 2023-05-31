d5e5125efbfa26e1342b481d8e19094e2ee1a2bd
Sreesanth46 <sreesanth46@gmail.com>
2023-05-31 10:00:13 +0530

    feat: caption component - text writing animation

---
+++ src/components/Caption.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="typing-demo">I write code</div>
+</template>
+
+<style>
+.typing-demo {
+    width: 22ch;
+    animation: typing 3s steps(22), blink 0.5s step-end infinite alternate;
+    white-space: nowrap;
+    overflow: hidden;
+    /* border-right: 3px solid; */
+    font-family: monospace;
+    font-size: 2em;
+}
+
+@keyframes typing {
+    from {
+        width: 0;
+    }
+}
+
+@keyframes blink {
+    50% {
+        border-color: transparent;
+    }
+}
+</style>
