6d362732ebfcee00a270e5d2659264a075c81d0d
Sreesanth46 <sreesanth46@gmail.com>
2023-06-23 10:23:57 +0530

    feat: global urls

---
+++ src/globals.js
@@ -0,0 +1,5 @@
+export const URLS = {
+    GITHUB: 'https://github.com/Sreesanth46',
+    LINKEDIN: 'https://www.linkedin.com/in/sreesanth46',
+    INSTAGRAM: 'https://www.instagram.com/sreesanth.sh',
+}
