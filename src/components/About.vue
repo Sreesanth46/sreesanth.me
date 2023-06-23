e0dd64743c5e25460835aa9f9619e13332e698f2
Sreesanth46 <sreesanth46@gmail.com>
2023-06-23 10:26:00 +0530

    fix: external links, feat: caption

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,22 +1,32 @@
+<script setup>
+import GithubIconVue from "../icons/GithubIcon.vue";
+import InstagramIconVue from "../icons/InstagramIcon.vue";
+import LinkedinIconVue from "../icons/LinkedinIcon.vue";
+import Caption from "./Caption.vue";
+import { URLS } from "../globals";
+
+const textProp = "Hey, I'm Sreesanth, a fullstack developer.";
+</script>
+        
 <template>
     <div class="">
         <h1 class="">Sreesanth</h1>
     </div>
     <article>
         <div class="">
-            <p>Hey, I'm Sreesanth, a fullstack developer.</p>
+            <Caption :type-prop="textProp" />
             <p>Find me on</p>
-            <p class="flex">
-                <GithubIconVue />
-                <InstagramIconVue />
-                <LinkedinIconVue />
+            <p class="grid gap-3">
+                <a :href="URLS.GITHUB">
+                    <GithubIconVue />
+                </a>
+                <a :href="URLS.INSTAGRAM">
+                    <InstagramIconVue />
+                </a>
+                <a :href="URLS.LINKEDIN">
+                    <LinkedinIconVue />
+                </a>
             </p>
         </div>
     </article>
 </template>
-
-<script setup>
-import GithubIconVue from "../icons/GithubIcon.vue";
-import InstagramIconVue from "../icons/InstagramIcon.vue";
-import LinkedinIconVue from "../icons/LinkedinIcon.vue";
-</script>
