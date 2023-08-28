b00d320fc1ecf5811779d44a37b8d2207cc7cbe7
Sreesanth <sreesanth46@gmail.com>
2023-08-28 22:07:53 +0530

    chore: darkMode - class, container padding

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+    darkMode: 'class',
     theme: {
         extend: {
             colors: {
@@ -8,14 +9,25 @@ module.exports = {
             }
         },
         fontFamily: {
-            Roboto: ["Roboto, sans-serif"]
+            Roboto: ['Roboto, sans-serif']
         },
         screens: {
-            sm: "480px",
-            md: "768px",
-            lg: "976px",
-            xl: "1440px"
-        }
+            sm: '480px',
+            md: '768px',
+            lg: '976px',
+            xl: '1440px'
+        },
+        container: {
+            padding: {
+                xs: '0.1rem',
+                sm: '2rem',
+                lg: '5rem',
+                xl: '12rem',
+                '2xl': '15rem',
+
+            },
+            center: true
+        },
     },
     plugins: [],
 }
