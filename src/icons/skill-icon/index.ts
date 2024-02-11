522c0f1190d41ca0ba026709b24ab29758767802
Sreesanth <sreesanth46@gmail.com>
2024-02-11 17:03:18 +0530

    feat: skill icons

---
+++ src/icons/skill-icon/index.ts
@@ -0,0 +1,13 @@
+import DjangoSkillIcon from './DjangoSkillIcon.vue';
+import NextSkillIcon from './NextSkillIcon.vue';
+import NodeSkillIcon from './NodeSkillIcon.vue';
+import ReactSkillIcon from './ReactSkillIcon.vue';
+import VueSkillIcon from './VueSkillIcon.vue';
+
+export {
+  DjangoSkillIcon,
+  NextSkillIcon,
+  NodeSkillIcon,
+  ReactSkillIcon,
+  VueSkillIcon
+};
