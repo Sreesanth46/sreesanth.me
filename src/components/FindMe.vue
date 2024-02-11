6324e3901a05da893a6f5d7b9c85f6f1f7cab86b
Sreesanth <sreesanth46@gmail.com>
2024-02-11 18:54:48 +0530

    feat: find me links

---
+++ src/components/FindMe.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps({
+  label: { type: String, required: true },
+  link: { type: String, required: false }
+});
+</script>
+
+<template>
+  <a :href="link" target="_blank" class="flex gap-1 underline">
+    <slot></slot>
+    <p>{{ label }}</p>
+  </a>
+</template>
