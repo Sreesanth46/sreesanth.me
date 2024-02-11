7c36dced4105a4ac4fc23e6d3ad141139651fe2d
Sreesanth <sreesanth46@gmail.com>
2024-02-11 18:54:58 +0530

    feat: footer

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,9 @@ import Plum from '~/components/Plum.vue';
   <div class="flex flex-col h-screen pb-2 px-6">
     <Plum />
     <Navbar />
-    <div class="flex-auto pt-10"><router-view></router-view></div>
+    <div class="flex-auto py-10"><router-view></router-view></div>
+    <footer class="flex justify-center py-6">
+      <p class="text-gray-600/30 text-xs">2023-PRESENT © Sreesanth</p>
+    </footer>
   </div>
 </template>
