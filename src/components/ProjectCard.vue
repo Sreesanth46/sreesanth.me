af193c6a70a085162509b1cc1faf71d58c0ff3d1
Sreesanth <sreesanth46@gmail.com>
2024-02-12 00:51:40 +0530

    feat: project card

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+defineProps({
+  title: { type: String, required: true },
+  link: { type: String, required: false },
+  descriptions: { type: String, required: false }
+});
+</script>
+
+<template>
+  <a
+    class="item relative flex items-center rounded-md gap-2 p-2 text-gray-700/70 hover:bg-gray-100 hover:text-gray-700"
+    target="_blank"
+    :href="link"
+    :title="title">
+    <div class="flex justify-center items-center">
+      <slot name="icon"></slot>
+    </div>
+    <div class="flex-auto">
+      <p class="text-normal font-medium">{{ title }}</p>
+      <p class="desc text-sm opacity-50 font-normal">
+        {{ descriptions }}
+      </p>
+    </div>
+  </a>
+</template>
--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -1,1 +1,20 @@
-<template>Projects</template>
+<script setup lang="ts">
+import ProjectCard from '~/components/ProjectCard.vue';
+import { ProjectList } from '~/globals';
+</script>
+
+<template>
+  <div class="sm:m-auto max-w-[75ch]">
+    <div class="flex flex-col">
+      <project-card
+        v-for="project in ProjectList"
+        :key="project.title"
+        :title="project.title"
+        :descriptions="project.description">
+        <template #icon>
+          <component :is="project.icon" />
+        </template>
+      </project-card>
+    </div>
+  </div>
+</template>
