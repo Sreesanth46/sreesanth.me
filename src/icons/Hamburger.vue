2d4386dc13735288dba91642b2fa2f57c04b5d89
Sreesanth <sreesanth46@gmail.com>
2024-02-11 12:07:24 +0530

    feat: icon

---
+++ src/icons/Hamburger.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import type { IconProps } from './interface';
+withDefaults(defineProps<IconProps>(), {
+  size: 24,
+  color: 'rgb(136, 136, 136)'
+});
+</script>
+
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    :width="size"
+    :height="size"
+    viewBox="0 0 24 24">
+    <path
+      fill="none"
+      :stroke="color"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M4 6h16M4 12h16M4 18h16" />
+  </svg>
+</template>
