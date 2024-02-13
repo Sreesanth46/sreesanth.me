ac1268e88388403187b3daecba1f781394c39262
Sreesanth <sreesanth46@gmail.com>
2024-02-13 22:34:40 +0530

    chore: icons

---
+++ src/icons/RightArrowOutlined.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    aria-hidden="true"
+    focusable="false"
+    width="1em"
+    height="1em"
+    preserveAspectRatio="xMidYMid meet"
+    viewBox="0 0 24 24"
+    data-icon="ri:arrow-right-up-line"
+    style="vertical-align: -0.125em; transform: rotate(360deg)">
+    <path
+      fill="currentColor"
+      d="m16.004 9.414l-8.607 8.607l-1.414-1.414L14.59 8H7.003V6h11v11h-2z"></path>
+  </svg>
+</template>
