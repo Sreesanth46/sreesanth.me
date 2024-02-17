76383d8ee55f69163b17b1d7b368214a13bf8d01
Sreesanth <sreesanth46@gmail.com>
2024-02-17 18:07:32 +0530

    feat: project card

--- src/components/ProjectExtraCard.vue
+++ src/components/ProjectExtraCard.vue
@@ -1,30 +1,29 @@
- <script setup lang="ts">
+<script setup lang="ts">
 import Github from '~/icons/Github.vue';
 import RightArrowOutlined from '~/icons/RightArrowOutlined.vue';
-defineProps({
-  title: { type: String, required: true },
-  link: { type: String, required: false },
-  descriptions: { type: String, required: false },
-});
+defineProps<{
+  title: string;
+  link?: string;
+  descriptions?: string;
+}>();
 </script>
 
 <template>
   <div
-    class="item relative flex items-center rounded-md gap-2 p-2 hover:bg-gray-100 hover:text-gray-950">
+    class="relative flex items-center rounded-md gap-2 p-2 hover:bg-gray-100 hover:text-gray-950">
     <div class="flex-auto">
-      <p class="flex gap-2 items-center">
-          <p class="text-normal font-semibold">{{ title }}</p>
-            <a 
-            class="flex gap-1 items-center" 
-            v-if="link"
-            :href="link"
-            target="_blank"
-            >
-                <Github class="h-3 w-3" />
-                <RightArrowOutlined />
-            </a>
-      </p>
-      <p class="desc text-sm opacity-50 font-normal">{{descriptions}}</p>
+      <div class="flex gap-2 items-center">
+        <p class="text-normal font-semibold">{{ title }}</p>
+        <a
+          class="flex gap-1 items-center"
+          v-if="link"
+          :href="link"
+          target="_blank">
+          <Github class="h-3 w-3" />
+          <RightArrowOutlined />
+        </a>
+      </div>
+      <p class="desc text-sm opacity-50 font-normal">{{ descriptions }}</p>
     </div>
   </div>
 </template>
