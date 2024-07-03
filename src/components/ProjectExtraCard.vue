397eca81ff23a4d0bad167205027c89908c364de
Sreesanth <sreesanth46@gmail.com>
2024-07-03 22:41:22 +0530

    style: updated hover styles and dark background

--- src/components/ProjectExtraCard.vue
+++ src/components/ProjectExtraCard.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import Github from '~/icons/Github.vue';
-import RightArrowOutlined from '~/icons/RightArrowOutlined.vue';
+import Github from "~/icons/Github.vue";
+import RightArrowOutlined from "~/icons/RightArrowOutlined.vue";
 defineProps<{
   title: string;
   link?: string;
@@ -10,7 +10,8 @@ defineProps<{
 
 <template>
   <div
-    class="relative flex items-center rounded-md gap-2 p-2 hover:bg-gray-100 hover:text-gray-950">
+    class="relative flex items-center rounded-md gap-2 p-2 hover:bg-gray-100/70 dark:hover:bg-black/40 dark:text-gray-300 dark:hover:text-white hover:text-gray-950 hover:cursor-pointer"
+  >
     <div class="flex-auto">
       <div class="flex gap-2 items-center">
         <p class="text-normal font-semibold">{{ title }}</p>
@@ -18,12 +19,15 @@ defineProps<{
           class="flex gap-1 items-center"
           v-if="link"
           :href="link"
-          target="_blank">
+          target="_blank"
+        >
           <Github class="h-3 w-3" />
           <RightArrowOutlined />
         </a>
       </div>
-      <p class="desc text-sm opacity-50 font-normal">{{ descriptions }}</p>
+      <p class="desc text-sm opacity-50 font-normal">
+        {{ descriptions }}
+      </p>
     </div>
   </div>
 </template>
