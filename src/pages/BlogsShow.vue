b29edb57ab80194b13c6181304bd59b4fecfa728
Sreesanth <sreesanth46@gmail.com>
2025-01-26 18:26:48 +0530

    feat: add loader skelton

--- src/pages/BlogsShow.vue
+++ src/pages/BlogsShow.vue
@@ -1,27 +1,33 @@
 <script setup lang="ts">
-import { computed, provide } from "vue";
-import { blogs } from "~/blogs";
-import Markdown from "~/components/Markdown.vue";
+import { computed, provide } from 'vue';
+import { blogs } from '~/blogs';
+import Markdown from '~/components/Markdown.vue';
 const { slug } = defineProps<{ slug: string }>();
 
 const blog = computed(() => {
-  return blogs.find((blog) => blog.name === slug);
+  return blogs.find(blog => blog.name === slug);
 });
 
-provide("readTime", blog.value?.readTime);
+provide('readTime', blog.value?.readTime);
 </script>
 <template>
   <Transition name="fade" mode="out-in">
     <Suspense>
-      <component
-        :is="Markdown"
-        :markdown="
-          blog?.url ??
-          `
-        <h4 class='text-center pt-6'>Oops! Not found</h4>
-        `
-        "
-      ></component>
+      <component :is="Markdown" :markdown="blog?.url ?? `Oops! Not found`" />
+      <template #fallback>
+        <div role="status" class="relative animate-pulse">
+          <div
+            class="h-8 max-w-96 bg-gray-200 rounded-md dark:bg-gray-900 mb-2" />
+          <div class="h-8 w-56 bg-gray-200 rounded-md dark:bg-gray-900 mb-4" />
+          <div class="h-4 bg-gray-200 rounded-md dark:bg-gray-900 w-48 mb-10" />
+
+          <div
+            class="h-6 bg-gray-200 rounded-md dark:bg-gray-900 mb-4"
+            v-for="item in Array.from(Array(5).keys())"
+            :key="item" />
+          <span class="sr-only">Loading...</span>
+        </div>
+      </template>
     </Suspense>
   </Transition>
 </template>
