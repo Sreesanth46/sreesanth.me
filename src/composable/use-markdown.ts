b89de58f0671de086272e4a82f8f0b644dae817d
Sreesanth <48680973+Sreesanth46@users.noreply.github.com>
2026-01-19 01:48:48 +0530

    refactor: enable html parsing (#66)

--- src/composable/use-markdown.ts
+++ src/composable/use-markdown.ts
@@ -1,7 +1,12 @@
 import Shiki from '@shikijs/markdown-it';
 import MarkdownIt from 'markdown-it';
 
-const md = MarkdownIt();
+const md = MarkdownIt({
+  html: true,
+  breaks: true,
+  linkify: true,
+  typographer: true
+});
 
 md.use(
   await Shiki({
