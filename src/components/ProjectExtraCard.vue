55528fc10152dff3a1e6e46123b12ab700194185
Sreesanth <sreesanth46@gmail.com>
2026-01-25 01:17:45 +0530

    chore: improve template formatting and add rel attributes for links

--- src/components/ProjectExtraCard.vue
+++ src/components/ProjectExtraCard.vue
@@ -11,15 +11,24 @@ defineProps<{
 
 <template>
   <div
-    class="relative flex items-center rounded-md gap-2 p-2 hover:bg-gray-100/70 dark:hover:bg-black/40 dark:text-gray-300 dark:hover:text-white hover:text-gray-950"
-  >
+    class="relative flex items-center rounded-md gap-2 p-2 hover:bg-gray-100/70 dark:hover:bg-black/40 dark:text-gray-300 dark:hover:text-white hover:text-gray-950">
     <div class="flex-auto">
       <div class="flex gap-2 items-center">
         <p class="text-normal font-semibold">{{ title }}</p>
-        <a class="flex gap-1 items-center" v-if="github" :href="github" target="_blank">
+        <a
+          class="flex gap-1 items-center"
+          v-if="github"
+          :href="github"
+          target="_blank"
+          rel="noopener noreferrer">
           <Github class="h-3 w-3" />
         </a>
-        <a class="flex gap-1 items-center" v-if="live" :href="live" target="_blank">
+        <a
+          class="flex gap-1 items-center"
+          v-if="live"
+          :href="live"
+          target="_blank"
+          rel="noopener noreferrer">
           <RightArrowOutlined />
         </a>
       </div>
